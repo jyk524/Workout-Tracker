4df6203cea9dca83b9c8ed8cc3336d3cf80fa2a6
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-08-20 19:08:26 -0400

    added server.js and api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+const Workout = db.Workout;
+
+module.exports = function (app) {
+  //create workout post
+  app.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  //add workout put
+  app.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, {
+      $push: { exercises: req.body },
+    })
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  //retrieve workouts? get
+  app.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
