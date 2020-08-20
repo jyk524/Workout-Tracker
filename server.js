5cc14b53182417d041fd20387265b907600001aa
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-08-20 19:08:10 -0400

    added server.js and api routes

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 3000
+
+// Middleware
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",{
+    useNewUrlParser: true,
+});
+
+// routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// 
+app.listen(3000, () => {
+    console.log("App running on port 3000!");
+});
+
