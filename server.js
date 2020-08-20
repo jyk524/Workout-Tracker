d419048483463bc09dd1d186097c57d5712e9d25
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-08-20 19:19:57 -0400

    modified server.js

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-// Dependencies
+//Dependencies
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const app = express();
 
-// PORT
-const PORT = process.env.PORT || 3000
+//PORT
+const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.urlencoded({ extended:true }));
+//application middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",{
+//connecting to the database
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://sullivan:sullivan25@ds025752.mlab.com:25752/heroku_64fk3f5d",
+  {
     useNewUrlParser: true,
-});
+    useUnifiedTopology: true,
+  }
+);
 
-// routes
+//Routes
 require("./routes/api-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
-// 
-app.listen(3000, () => {
-    console.log("App running on port 3000!");
+// Listen on port 3000
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
-
