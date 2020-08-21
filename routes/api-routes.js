224f9126a5a01391392bce72ed8c326efb35169c
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-08-20 20:50:10 -0400

    added stats page api with dates

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -37,5 +37,16 @@ module.exports = function (app) {
       });
   });
   
+   // GET stats page
+   app.get("/api/workouts/range", (req, res) => {
+    const start = new Date().setDate(new Date().getDate() - 7);
+    Workout.find({ day: { $gte: start, $lte: Date.now() } })
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
 };
