0c4b15ed660a71c042d19fc23d226b128468ac2f
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-08-20 20:34:12 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -14,15 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-//connecting to the database
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb://jyk524:mymlabp4ss@ds159997.mlab.com:59997/heroku_h1m6jtk9",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+// connecting to the database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 //Routes
 require("./routes/api-routes.js")(app);
